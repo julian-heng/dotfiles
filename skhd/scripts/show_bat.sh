e6b10fae8245ff517177b45c9b9a611ca8f473b6
Julian Heng <julianhengwl@gmail.com>
2018-06-16 00:47:42 +0800

    Skhd: Split show_bat

--- skhd/scripts/show_bat.sh
+++ skhd/scripts/show_bat.sh
@@ -1,46 +1,43 @@
 #!/usr/bin/env bash
 # shellcheck disable=1004,1090
 
-function get_bat_info
+function get_bat_state
 {
-    local bat_state
-    local bat_percent
-    local bat_time
-    local bat_cycles
-    local bat_condition
+    : "$(awk '/drawing/ {print $4}' < <(printf "%s\n" "${bat_cache[@]}"))"
+    : "${_//\'}"
+    printf "%s" "${_}"
+}
 
-    read -r bat_state \
-            bat_percent \
-            bat_time \
-            bat_cycles \
-            bat_condition \
-            < <(awk '
-                    /drawing/ { a=$4 }
-                    /id=/ { b=$3; c=$5 }
-                    /Cycle Count/ { d=$3 }
-                    /Condition/ { e=$2 }
-                    END { 
-                        printf "%s %s %s %s %s", \
-                        a, b, c, d, e 
-                    }' <(pmset -g batt; system_profiler SPPowerDataType))
+function get_bat_percent
+{
+    : "$(awk '/id=/ {print $3}' < <(printf "%s\n" "${bat_cache[@]}"))"
+    : ${_//;}
+    printf "%s" "${_}"
+}
 
-    bat_percent="${bat_percent//;/}"
-    bat_state="${bat_state//\'/}"
-
-    if [[ "${bat_time}" =~ ^(\(no|charge;)$ ]]; then
-        bat_time="Unknown"
-    elif [[ "${bat_time}" == "0:00"* && "${bat_state}" == "AC" ]]; then
-        bat_time="Fully Charged"
+function get_bat_time
+{
+    : "$(awk '/id=/ {print $5}' < <(printf "%s\n" "${bat_cache[@]}"))"
+    if [[ "${_}" =~ ^(\(no|charge;)$ ]]; then
+        : "Unknown"
+    elif [[ "${_}" == "0:00"* && "${bat_state}" == "AC" ]]; then
+        : "Fully Charged"
     else
-        bat_time+=" remaining"
+        : "${_} remaining"
     fi
+    printf "%s" "${_}"
+}
 
-    printf "%s;%s;%s;%s;%s" \
-        "${bat_state}" \
-        "${bat_percent}" \
-        "${bat_time}" \
-        "${bat_cycles}" \
-        "${bat_condition}"
+function get_bat_cycles
+{
+    : "$(awk '/Cycle Count/ {print $3}' < <(printf "%s\n" "${bat_cache[@]}"))"
+    printf "%s" "${_}"
+}
+
+function get_bat_condition
+{
+    : "$(awk '/Condition/ {print $2}' < <(printf "%s\n" "${bat_cache[@]}"))"
+    printf "%s" "${_}"
 }
 
 function main
@@ -49,13 +46,13 @@ function main
         && source "${BASH_SOURCE[0]//${0##*/}/}format.sh"; } \
             && exit 1
 
-    IFS=";" \
-    read -r bat_state \
-            bat_percent \
-            bat_time \
-            bat_cycles \
-            bat_condition \
-            < <(get_bat_info)
+    mapfile -t bat_cache < <(pmset -g batt; system_profiler SPPowerDataType)
+
+    bat_state="$(get_bat_state)"
+    bat_percent="$(get_bat_percent)"
+    bat_time="$(get_bat_time)"
+    bat_cycles="$(get_bat_cycles)"
+    bat_condition="$(get_bat_condition)"
 
     title_parts=(
         "Battery" "(" "${bat_percent}" ")"
