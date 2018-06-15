b7c81b12f7272d0bb87738c68fca6d726524aa5d
Julian Heng <julianhengwl@gmail.com>
2018-06-16 02:25:28 +0800

    Skhd: Remove unused variable in show_disk.sh

--- skhd/scripts/show_disk.sh
+++ skhd/scripts/show_disk.sh
@@ -11,12 +11,11 @@ function spacify
 function get_search
 {
     local search="$1"
-    local -a disk_cache
-    mapfile -t disk_cache <<< "${@:2}"
-
     local count="0"
     local match="false"
-    local default_disk
+
+    local -a disk_cache
+    mapfile -t disk_cache <<< "${@:2}"
 
     if [[ "${search}" ]]; then
         while [[ "${match}" != "true" ]] && ((count < ${#disk_cache[@]})); do
@@ -44,79 +43,93 @@ function get_df_output
     local df_line
     local -a disk_cache
 
-    while read -r df_line && [[ "${df_line}" != *"TimeMachine"* ]]; do
-        disk_cache+=("${df_line}")
+    while read -r df_line; do
+        [[ "${df_line}" != *"TimeMachine"* ]] && disk_cache+=("${df_line}")
     done < <(df -P -k)
     printf "%s\\n" "${disk_cache[@]}"
 }
 
-function get_disk
+function get_diskutil_out
 {
-    local -a disk_cache
-    local search
+    local -a diskutil_out
+    mapfile -t diskutil_out < <(diskutil info "$1")
+    printf "%s\\n" "${diskutil_out[@]}"
+}
 
-    local disk_device
-    local disk_capacity
-    local disk_used
-    local disk_percent
-    local disk_name
-    local disk_part
-    local disk_mount
+function get_disk_device
+{
+    : "$(awk -v disk="$1" \
+        '$0 ~ disk {print $1; exit}' < \
+            <(printf "%s\\n" "${@:2}"))"
+    printf "%s" "${_}"
+}
 
-    disk_cache=("$(get_df_output "$@")")
-    ! search="$(get_search "${@:-}" "${disk_cache[@]}")" && return 1
+function get_disk_capacity
+{
+    : "$(awk -v disk="$1" \
+        '$0 ~ disk {printf "%0.2f", $2 / (1024 ^ 2); exit}' < \
+            <(printf "%s\\n" "${@:2}"))"
+    printf "%s" "${_}"
+}
 
-    read -r disk_device \
-            disk_capacity \
-            disk_used \
-            disk_percent \
-            < <(awk -v disk="${search}" \
-                    '$0 ~ disk { 
-                        a = $1
-                        b = $2 / (1024 ^ 2)
-                        c = $3 / (1024 ^ 2)
-                        d = c / b * 100
-                    }
-                    END {
-                        printf "%s %0.2f %0.2f %0.2f", \
-                        a, b, c, d
-                    }' <<< "${disk_cache[@]}")
+function get_disk_used
+{
+    : "$(awk -v disk="$1" \
+        '$0 ~ disk {printf "%0.2f",  $3 / (1024 ^ 2); exit}' < \
+            <(printf "%s\\n" "${@:2}"))"
+    printf "%s" "${_}"
+}
 
-    read -r disk_name \
-            disk_part \
-            disk_mount \
-            < <(awk '
-                    /Volume Name/ {
-                        for(i = 3; i <= NF; i++) {
-                            a = a":"$i
-                        }
-                    }
-                    /File System Personality:/ {
-                        for(i = 4; i <= NF; i++) {
-                            b = b":"$i
-                        }
-                    }
-                    /Mount Point:/ {
-                        for(i = 3; i <= NF; i++) {
-                            c = c":"$i
-                        }
-                    }
-                    END {
-                        printf "%s %s %s", a, b, c
-                    }' <(diskutil info "${search}"))
+function get_disk_percent
+{
+    : "$(awk -v a="$1" -v b="$2" \
+        'BEGIN {printf "%0.2f", (a / b) * 100}')"
+    printf "%s" "${_}"
+}
 
-    disk_name="$(spacify "${disk_name}")"
-    disk_part="$(spacify "${disk_part}")"
-    disk_mount="$(spacify "${disk_mount}")"
+function get_disk_name
+{
+    : "$(awk '
+        /Volume Name/ {
+            for (i = 3; i <= NF; i++) {
+                a = a":"$i
+            }
+        }
+        END {
+            print a
+        }' < <(printf "%s\\n" "$@"))"
+    : "$(spacify "${_}")"
+    printf "%s" "${_}"
+}
 
-    printf "%s;%s;%s;%s;%s;%s;%s" \
-        "${disk_device}" \
-        "${disk_capacity}" \
-        "${disk_used}" \
-        "${disk_percent}" \
-        "${disk_name}" \
-        "${disk_part}" \
-        "${disk_mount}"
+function get_disk_part
+{
+    : "$(awk '
+        /File System Personality:/ {
+            for (i = 4; i <= NF; i++) {
+                a = a":"$i
+            }
+        }
+        END {
+            print a
+        }' < <(printf "%s\\n" "$@"))"
+    : "$(spacify "${_}")"
+    printf "%s" "${_}"
+}
+
+function get_disk_mount
+{
+    : "$(awk '
+        /Mount Point:/ {
+            for (i = 3; i <= NF; i++) {
+                a = a":"$i
+            }
+        }
+        END {
+            print a
+        }' < <(printf "%s\\n" "$@"))"
+    : "$(spacify "${_}")"
+    printf "%s" "${_}"
 }
 
 function main
@@ -125,15 +138,17 @@ function main
         && source "${BASH_SOURCE[0]//${0##*/}/}format.sh"; } \
             && exit 1
 
-    IFS=";" \
-    read -r disk_device \
-            disk_capacity \
-            disk_used \
-            disk_percent \
-            disk_name \
-            disk_part \
-            disk_mount \
-            < <(get_disk "$@")
+    disk_cache=("$(get_df_output "$@")")
+    ! search="$(get_search "${@:-}" "${disk_cache[@]}")" && return 1
+    diskutil_out=("$(get_diskutil_out "${search}")")
+
+    disk_device="$(get_disk_device "${search}" "${disk_cache[@]}")"
+    disk_capacity="$(get_disk_capacity "${search}" "${disk_cache[@]}")"
+    disk_used="$(get_disk_used "${search}" "${disk_cache[@]}")"
+    disk_percent="$(get_disk_percent "${disk_used}" "${disk_capacity}")"
+    disk_name="$(get_disk_name "${diskutil_out[@]}")"
+    disk_part="$(get_disk_part "${diskutil_out[@]}") "
+    disk_mount="$(get_disk_mount "${diskutil_out[@]}")"
 
     [[ "${disk_device}" == "" \
     || "${disk_capacity}" == "0.00" \
