e418796c90536a4e92dcd0116af084be69c88b23
Julian Heng <julian@julians-mbp.home>
2018-06-27 23:45:15 +0800

    Skhd: Fix format.sh

--- skhd/scripts/format.sh
+++ skhd/scripts/format.sh
@@ -3,54 +3,60 @@
 function format
 (
     while (($# > 0)); do
+        word=""
+        count="0"
         case "$1" in
             "(")
-                 if [[ "$2" ]]; then
-                     while read -r i && [[ "${i}" != ")" ]]; do
-                         ((count++))
-                     done < <(printf "%s\\n" "$@")
-                     ((count++))
-                     : "${*:1:${count}}"
-                     : " ${_// }"
-                 else
-                     while read -r i && [[ "${i}" != "|" ]]; do
-                         ((count++))
-                     done < <(printf "%s\\n" "$@")
-                     ((count++))
-                     : ""
-                 fi
+                if [[ "$2" ]]; then
+                    while read -r i; do
+                        if [[ "${i}" != ")" ]]; then
+                            ((count++))
+                            word+="${i}"
+                        else
+                            break
+                        fi
+                    done < <(printf "%s\\n" "$@")
+                    ((count++))
+                    word=" ${word})"
+                else
+                    until read -r i && [[ "${i}" != ")" ]]; do
+                        ((count++))
+                    done < <(printf "%s\\n""$@")
+                fi
             ;;
             "|")
                 if [[ "$2" ]]; then
-                     word="|"
-                     shift
-                     while read -r i && [[ "${i}" != "(" && "${i}" != "|" ]]; do
-                         if [[ "${i}" ]]; then
-                             if [[ "${i}" == "%" ]]; then
-                                 word+="${i}"
-                             else
-                                 word+=" ${i}"
-                             fi
-                         fi
-                         ((count++))
-                     done < <(printf "%s\\n" "$@")
-                     : " ${word}"
-                 else
-                    while read -r i && [[ "${i}" != "|" ]]; do
+                    word="|"
+                    count="1"
+                    while read -r i; do
+                        if [[ "${i}" =~ ^(\||\()$ ]]; then
+                            break
+                        elif [[ "${i}" == "%" ]]; then
+                            ((count++))
+                            word="${word}${i}"
+                        else
+                            ((count++))
+                            word+="${word} ${i}"
+                        fi
+                    done < <(printf "%s\\n" "$@")
+                    word=" ${word%*[[:space:]]}"
+                else
+                    until read -r i && [[ "${i}" != "|" ]]; do
                         ((count++))
                     done < <(printf "%s\\n" "$@")
-                    : ""
-                 fi
+                fi
             ;;
             *)
                 ((count++))
-                [[ ! "$1" || "$1" == "%" || ! "${line}" ]] && : "$1" || \
-                    : " $1"
+                if [[ ! "$1" || "$1" == "%" || ! "${line}" ]]; then
+                    word="$1"
+                else
+                    word=" $1"
+                fi
             ;;
         esac
-        line+="${_}"
+        line+="${word}"
         shift "${count}"
-        count="0"
     done
     printf "%s" "${line}"
 )
