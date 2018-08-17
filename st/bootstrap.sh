a164c601502a281cbeef6a663612ef91165b7a34
julian-heng <julianhengwl@gmail.com>
2018-08-17 17:34:41 +0800

    st: Added skipping actions

--- st/bootstrap.sh
+++ st/bootstrap.sh
@@ -44,15 +44,14 @@ clone()
     fi
 
     if [[ ! -e "${dir}/st" ]]; then
-        clone="true"
         clone_dir="${dir}/st"
         printf "%s\\n" "Cloning \"git://git.suckless.org/st\" to ${clone_dir}"
     elif check_git_repo_url "${dir}/st" "git://git.suckless.org/st"; then
-        clone="false"
+        clone="${clone:-false}"
         clone_dir="${dir}/st"
         printf "%s\\n" "Git repo for st already exist"
     else
-        clone="true"
+        clone="${clone:-true}"
         printf -v clone_dir "%s%(%d-%m-%Y-%T)T" "${dir}/st-" "-1"
         printf "%s\\n" "Directory is not empty, using ${clone_dir}"
     fi
@@ -63,6 +62,9 @@ clone()
 
 copy_config()
 {
+    [[ "${copy}" == "false" ]] && \
+        return 0
+
     [[ -e "${clone_dir}/config.h" ]] && {
         printf "%s\\n" "Warning: Config file already exist. Backing up to config.h.bootstrap"
         mv "${clone_dir}/config.h" "${clone_dir}/config.h.bootstrap"
@@ -74,6 +76,9 @@ copy_config()
 
 apply_patches()
 {
+    [[ "${patch}" == "false" ]] && \
+        return 0
+
     urls=(
         "https://st.suckless.org/patches/alpha/st-alpha-0.8.1.diff"
         "https://st.suckless.org/patches/scrollback/st-scrollback-0.8.diff"
@@ -95,6 +100,9 @@ apply_patches()
 
 make_and_install()
 {
+    [[ "${install}" == "false" ]] && \
+        return 0
+
     cd "${clone_dir}" || exit 1
     make
     sudo make install
@@ -115,8 +123,22 @@ Keywords=shell;prompt;command;commandline;cmd;'
     printf "%s" "${desktop_entry}" > "${HOME}/.local/share/applications/st.desktop"
 }
 
+get_args()
+{
+    while (($# > 0)); do
+        case "$1" in
+            "-scl"|"--skip-clone")  clone="false" ;;
+            "-sc"|"--skip-copy")    copy="false" ;;
+            "-sp"|"--skip-patch")   patch="false" ;;
+            "-si"|"--skip-install") install="false" ;;
+        esac
+        shift
+    done
+}
+
 main()
 {
+    get_args "$@"
     check_apps
     clone
     copy_config
