545e5acb2e12fd1466f775d0d41d8fbd30a8f8e3
julian-heng <julianhengwl@gmail.com>
2018-11-02 16:25:59 +0800

    Scripts: Add fallback wifi name, cpu fan and temperataure to right.sh

--- scripts/mac/ubersicht/right.sh
+++ scripts/mac/ubersicht/right.sh
@@ -25,6 +25,22 @@ get_cpu_load()
     load_avg="${load_avg/' }'}"
 }
 
+get_fan_temp()
+{
+    temp_path="/usr/local/bin/osx-cpu-temp"
+    type -p "${temp_path}" 2>&1 > /dev/null && {
+        awk_script='
+            /CPU/ { a = $2 }
+            /Fan [0-9]/ { b = $2 }
+            END {
+                printf "%s %s", a, b
+            }'
+        read -r temp \
+                fan \
+                < <(awk "${awk_script}" <("${temp_path}" -f -c))
+    }
+}
+
 get_mem_info()
 {
     awk_script='
@@ -51,10 +67,13 @@ get_wifi()
         /SSID:/ { name = $2 }
         END {
             printf "%s", name
-        }
-    '
+        }'
+
     wifi_name="$(awk -F":" "${awk_script}" \
         <(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport --getinfo))"
+    wifi_name="$(trim "${wifi_name}")"
+    [[ ! "${wifi_name}" ]] && \
+        wifi_name="Not Connected"
 }
 
 get_bat_info()
@@ -73,7 +92,7 @@ get_bat_info()
     bat_info="${bat_percent}%"
 
     [[ "${bat_time}" != "0:00" && "${bat_time}" != "(no" ]] && \
-        bat_info+=" ${bat_time}"
+        bat_info+=" | ${bat_time}"
 
 }
 
@@ -86,17 +105,29 @@ get_date_time()
 main()
 {
     get_cpu_load
+    get_fan_temp
     get_mem_info
     get_wifi
     get_bat_info
     get_date_time
 
-    printf -v out "/ %s " \
-        "${load_avg}" \
+    [[ "${fan}" ]] && \
+        cpu_str+=" | ${fan} RPM"
+
+    [[ "${temp}" ]] && \
+        cpu_str+=" | ${temp}"
+
+    cpu_str+=" | ${load_avg}"
+
+    [[ "${cpu_str:0:2}" == " |" ]] && \
+        cpu_str="${cpu_str##' |'}"
+
+    printf -v out "[ %s ] " \
+        "${cpu_str}" \
         "${mem_used} GiB | ${mem_total} GiB" \
         "${wifi_name}" \
         "${bat_info}" \
-        "${date} ${time}"
+        "${date} | ${time}"
 
     out="$(trim "${out#\/}")"
     printf "%s" "${out}"
