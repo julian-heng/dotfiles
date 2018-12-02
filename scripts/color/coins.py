a407aed77f3be2a64e82a0f1f5a55b701d15263a
julian-heng <julianhengwl@gmail.com>
2018-12-02 13:27:23 +0800

    Scripts: Consistent quotes

--- scripts/color/coins.py
+++ scripts/color/coins.py
@@ -20,9 +20,9 @@ def print_box(x1, y1, x2, y2, x_off, y_off, col, pat):
     x1 += x_off
     y1 += y_off
 
-    print('\033[{};{}H{}'.format(y1, x1, col))
+    print("\033[{};{}H{}".format(y1, x1, col))
     for i in range(length_y):
-        print('\033[{};{}H{}'.format(y1 + i, x1, pat * length_x))
+        print("\033[{};{}H{}".format(y1 + i, x1, pat * length_x))
 
 def main():
     c = colors()
@@ -33,23 +33,23 @@ def main():
         x_off = 14 * count
         count += 1
 
-        print_box(5, 3, 12, 12, x_off, 0, color, '█')
-        print_box(4, 4, 13, 11, x_off, 0, color, '█')
-        print_box(13, 6, 13, 9, x_off, 0, color, '█')
-        print_box(7, 12, 10, 12, x_off, 0, color, '█')
+        print_box(5, 3, 12, 12, x_off, 0, color, "█")
+        print_box(4, 4, 13, 11, x_off, 0, color, "█")
+        print_box(13, 6, 13, 9, x_off, 0, color, "█")
+        print_box(7, 12, 10, 12, x_off, 0, color, "█")
 
-        print_box(10, 4, 10, 11, x_off, 0, c.fg[0], '█')
-        print_box(7, 10, 10, 10, x_off, 0, c.fg[0], '█')
+        print_box(10, 4, 10, 11, x_off, 0, c.fg[0], "█")
+        print_box(7, 10, 10, 10, x_off, 0, c.fg[0], "█")
 
-        print_box(7, 2, 10, 2, x_off, 0, c.fg[7], '█')
-        print_box(5, 3, 7, 3, x_off, 0, c.fg[7], '█')
-        print_box(4, 4, 5, 4, x_off, 0, c.fg[7], '█')
-        print_box(7, 4, 10, 4, x_off, 0, c.fg[7], '█')
-        print_box(3, 5, 3, 9, x_off, 0, c.fg[7], '█')
-        print_box(7, 4, 7, 10, x_off, 0, c.fg[7], '█')
-        print_box(4, 9, 4, 11, x_off, 0, c.fg[7], '█')
-        print_box(5, 11, 5, 11, x_off, 0, c.fg[7], '█')
+        print_box(7, 2, 10, 2, x_off, 0, c.fg[7], "█")
+        print_box(5, 3, 7, 3, x_off, 0, c.fg[7], "█")
+        print_box(4, 4, 5, 4, x_off, 0, c.fg[7], "█")
+        print_box(7, 4, 10, 4, x_off, 0, c.fg[7], "█")
+        print_box(3, 5, 3, 9, x_off, 0, c.fg[7], "█")
+        print_box(7, 4, 7, 10, x_off, 0, c.fg[7], "█")
+        print_box(4, 9, 4, 11, x_off, 0, c.fg[7], "█")
+        print_box(5, 11, 5, 11, x_off, 0, c.fg[7], "█")
 
-    print('{}\n'.format(c.reset))
+    print("{}\n".format(c.reset))
 
 main()
