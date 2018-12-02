48962c4fe56399bc33bf0e82ed0b3dd1b13e5b2d
julian-heng <julianhengwl@gmail.com>
2018-12-02 12:11:01 +0800

    Scripts: Remove f-string usage

--- scripts/color/coins.py
+++ scripts/color/coins.py
@@ -49,10 +49,10 @@ def print_box(x1, y1, x2, y2, x_off, y_off, pat):
     length_y = length_y if length_y != 0 else length_y + 1
     x1 += x_off
     y1 += y_off
-    print(f"\033[{y1};{x1}H")
+    print('\033[{};{}H'.format(y1, x1))
     for i in range(0, length_y, 1):
         for j in range(0, length_x, 1):
-            print(f"\033[{y1 + i};{x1 + j}H{pat}")
+            print('\033[{};{}H{}'.format(y1 + i, x1 + j, pat))
 
 def main():
     c = colors()
@@ -63,23 +63,23 @@ def main():
         x_off = 14 * count
         count += 1
 
-        print_box(5, 3, 12, 12, x_off, 0, f"{color}█")
-        print_box(4, 4, 13, 11, x_off, 0, f"{color}█")
-        print_box(13, 6, 13, 9, x_off, 0, f"{color}█")
-        print_box(7, 12, 10, 12, x_off, 0, f"{color}█")
+        print_box(5, 3, 12, 12, x_off, 0, '{}{}'.format(color, '█'))
+        print_box(4, 4, 13, 11, x_off, 0, '{}{}'.format(color, '█'))
+        print_box(13, 6, 13, 9, x_off, 0, '{}{}'.format(color, '█'))
+        print_box(7, 12, 10, 12, x_off, 0, '{}{}'.format(color, '█'))
 
-        print_box(10, 4, 10, 11, x_off, 0, f"{c.fg_black}█")
-        print_box(7, 10, 10, 10, x_off, 0, f"{c.fg_black}█")
+        print_box(10, 4, 10, 11, x_off, 0, '{}{}'.format(c.fg_black, '█'))
+        print_box(7, 10, 10, 10, x_off, 0, '{}{}'.format(c.fg_black, '█'))
 
-        print_box(7, 2, 10, 2, x_off, 0, f"{c.fg_white}█")
-        print_box(5, 3, 7, 3, x_off, 0, f"{c.fg_white}█")
-        print_box(4, 4, 5, 4, x_off, 0, f"{c.fg_white}█")
-        print_box(7, 4, 10, 4, x_off, 0, f"{c.fg_white}█")
-        print_box(3, 5, 3, 9, x_off, 0, f"{c.fg_white}█")
-        print_box(7, 4, 7, 10, x_off, 0, f"{c.fg_white}█")
-        print_box(4, 9, 4, 11, x_off, 0, f"{c.fg_white}█")
-        print_box(5, 11, 5, 11, x_off, 0, f"{c.fg_white}█")
+        print_box(7, 2, 10, 2, x_off, 0, '{}{}'.format(c.fg_white, '█'))
+        print_box(5, 3, 7, 3, x_off, 0, '{}{}'.format(c.fg_white, '█'))
+        print_box(4, 4, 5, 4, x_off, 0, '{}{}'.format(c.fg_white, '█'))
+        print_box(7, 4, 10, 4, x_off, 0, '{}{}'.format(c.fg_white, '█'))
+        print_box(3, 5, 3, 9, x_off, 0, '{}{}'.format(c.fg_white, '█'))
+        print_box(7, 4, 7, 10, x_off, 0, '{}{}'.format(c.fg_white, '█'))
+        print_box(4, 9, 4, 11, x_off, 0, '{}{}'.format(c.fg_white, '█'))
+        print_box(5, 11, 5, 11, x_off, 0, '{}{}'.format(c.fg_white, '█'))
 
-    print(f"{c.reset}\n")
+    print('{}\n'.format(c.reset))
 
 main()
