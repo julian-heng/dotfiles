52beb934b70dfed1559db120753193daa6123c28
julian-heng <julianhengwl@gmail.com>
2019-01-25 08:50:27 +0800

    Tmux: Simplify info status

--- tmux/info.bash
+++ tmux/info.bash
@@ -4,29 +4,23 @@ main()
 {
     script_dir="${HOME}/.dotfiles/scripts/info"
 
-    mapfile -t cpu < <(bash "${script_dir}/show_cpu.bash" load temp fan)
-    mapfile -t disk < <(bash "${script_dir}/show_disk.bash" disk_device disk_percent)
-    mem="$(bash "${script_dir}/show_mem.bash" mem_percent)"
-    bat="$(bash "${script_dir}/show_bat.bash" bat_percent 2> /dev/null)"
+    mapfile -t cpu < <(bash "${script_dir}/show_cpu.bash" load temp)
+    mapfile -t mem < <(bash "${script_dir}/show_mem.bash" mem_used mem_total)
+
+    mem[0]="${mem[0]/' MiB'}"
+    mem[1]="${mem[1]/' MiB'}"
+
+    printf -v mem[0] "%.*f" "2" "${mem[0]:0:(${#mem[0]} - 3)}.${mem[0]:(-3)}"
+    printf -v mem[1] "%.*f" "2" "${mem[1]:0:(${#mem[1]} - 3)}.${mem[1]:(-3)}"
 
     [[ "${cpu[0]}" ]] && cpu_out="${cpu_out}${cpu[0]}"
     [[ "${cpu[1]}" ]] && cpu_out="${cpu_out} | ${cpu[1]}"
-    [[ "${cpu[2]}" ]] && cpu_out="${cpu_out} | ${cpu[2]}"
-
-    [[ "${mem}" ]] && mem_out="Mem: ${mem}"
-
-    [[ "${disk[0]}" ]] && disk_out="${disk[0]##*/}"
-    [[ "${disk[1]}" ]] && \
-        printf -v disk_out "%s: %.*f%%" "${disk_out}" "0" "${disk[1]/'%'}"
-
-    [[ "${bat}" ]] && bat_out="Bat: ${bat}"
+    [[ "${mem[*]}" ]] && mem_out="${mem[0]} MiB / ${mem[1]} MiB"
 
     printf -v time_out "%(%a, %d %h)T | %(%H:%M)T" "-1"
 
     [[ "${cpu_out}" ]]   && printf "| %s " "${cpu_out}"
     [[ "${mem_out}" ]]   && printf "| %s " "${mem_out}"
-    [[ "${disk_out}" ]]  && printf "| %s " "${disk_out}"
-    [[ "${bat_out}" ]]   && printf "| %s " "${bat_out}"
     [[ "${time_out}" ]]  && printf "| %s " "${time_out}"
 
     printf "|"
