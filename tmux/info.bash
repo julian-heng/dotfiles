5d5f91c6b0b75b59614d02c1119b4526d0050fc7
julian-heng <julianhengwl@gmail.com>
2019-01-27 15:46:30 +0800

    Tmux: Fix memory display

--- tmux/info.bash
+++ tmux/info.bash
@@ -10,12 +10,18 @@ main()
     mem[0]="${mem[0]/' MiB'}"
     mem[1]="${mem[1]/' MiB'}"
 
-    printf -v mem[0] "%.*f" "2" "${mem[0]:0:(${#mem[0]} - 3)}.${mem[0]:(-3)}"
-    printf -v mem[1] "%.*f" "2" "${mem[1]:0:(${#mem[1]} - 3)}.${mem[1]:(-3)}"
+    awk_script='BEGIN { printf "%f %f", a / 1024, b / 1024 }'
+
+    read -r mem[0] \
+            mem[1] \
+            < <(awk -v a="${mem[0]}" -v b="${mem[1]}" "${awk_script}")
+
+    printf -v mem[0] "%.*f" "2" "${mem[0]}"
+    printf -v mem[1] "%.*f" "2" "${mem[1]}"
 
     [[ "${cpu[0]}" ]] && cpu_out="${cpu_out}${cpu[0]}"
     [[ "${cpu[1]}" ]] && cpu_out="${cpu_out} | ${cpu[1]}"
-    [[ "${mem[*]}" ]] && mem_out="${mem[0]} GiB / ${mem[1]} GiB"
+    [[ "${mem[*]}" ]] && mem_out="${mem[0]/'.00'} GiB / ${mem[1]/'.00'} GiB"
 
     printf -v time_out "%(%a, %d %h)T | %(%H:%M)T" "-1"
 
