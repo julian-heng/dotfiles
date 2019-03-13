abaf5aafda78e04d582d8a5775e0a9e4641b9b87
julian-heng <julianhengwl@gmail.com>
2019-03-13 13:38:05 +0800

    Ubersicht: Add volume and brightness level

--- ubersicht/info.bash
+++ ubersicht/info.bash
@@ -9,7 +9,17 @@ main()
     mem="$("${bash_exec}" "${script_dir}/show_mem" mem_percent)"
     mapfile -t disk < <("${bash_exec}" "${script_dir}/show_disk" disk_device disk_percent)
     mapfile -t bat < <("${bash_exec}" "${script_dir}/show_bat" bat_percent bat_time)
+    vol="$(/usr/bin/osascript -e 'output volume of (get volume settings)')"
 
+    while [[ ! "${bright}" ]] && read -r line; do
+        [[ "${line}" =~ IODisplayParameters ]] && {
+            bright="${line//\"}"
+            bright="${bright##*',brightness={'}"
+            bright="${bright%%"},"*}"
+            IFS="=," read -r _ max _ _ _ val <<< "${bright}"
+            bright="$(awk -v a="${val}" -v b="${max}" 'BEGIN { printf "%d", (a / b) * 100}')"
+        }
+    done < <(ioreg -rc AppleBacklightDisplay)
 
     wifi_name="$("${bash_exec}" "${script_dir}/show_net" network_ssid)"
     [[ ! "${wifi_name}" ]] && \
@@ -26,9 +36,17 @@ main()
         printf -v bat_out "[ Bat: %s | %s ]" "${bat[@]}"
     fi
 
+    printf -v vol_bright_out "[ vol: %s%% | scr: %s%% ]" "${vol}" "${bright}"
     printf -v time_out "[ %(%a, %d %h)T | %(%H:%M)T ]" "-1"
 
-    printf "%s %s %s %s %s %s" "${cpu_out}" "${mem_out}" "${disk_out}" "${bat_out}" "${wifi_out}" "${time_out}"
+    printf "%s " \
+        "${cpu_out}" \
+        "${mem_out}" \
+        "${disk_out}" \
+        "${bat_out}" \
+        "${wifi_out}" \
+        "${vol_bright_out}" \
+        "${time_out}"
 }
 
 main
