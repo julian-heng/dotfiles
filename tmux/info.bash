9cad00a6972a038a7eb46e31abfe5759dc78b37f
julian-heng <julianhengwl@gmail.com>
2020-07-11 18:30:33 +0800

    tmux: Update info.bash script

--- tmux/info.bash
+++ tmux/info.bash
@@ -8,14 +8,49 @@ get_window_size()
 
 get_status_length()
 {
-    # Assuming 3 characters in between every entry
-    status_length="2"
-    while (($# > 0)); do
-        ((status_length += ${#1}))
-        shift
-    done
+    printf -v tmp "%s" "${*}"
+    length="$((${#tmp} + (2 * (${#*} + 1))))"
+    printf "%d" "${#tmp}"
+}
 
-    printf "%d" "${status_length}"
+use_sys_line()
+{
+    out_fmt=(
+        $'| {cpu.load_avg}{cpu.temp? | {}°C}{mem[round=0].percent? | Mem: {mem[prefix=GiB,round=0].used} ({}%)}{disk[round=0].percent? | {disk[short_dev].dev}: {disk[prefix=GiB,round=0].used} ({}%)} | {date.date} | {date.time} |'
+        $'| {cpu[load_short].load_avg}{cpu.temp? | {}°C}{mem[round=0].percent? | Mem: {}%}{disk[round=0].percent? | Disk: {}%} | {date.date} | {date.time} |'
+    )
+
+    mapfile -t out < <(sys-line "${out_fmt[@]}")
+    printf "%s" "${out[$((${#out[0]} < (window_size / 2) ? 0 : 1))]}"
+}
+
+use_show_scripts()
+{
+    script_dir="$(type -p show_cpu)"
+    script_dir="${script_dir%/*}"
+    script_dir="${script_dir:-${HOME}/.dotfiles/scripts/info}"
+
+    mapfile -t cpu_info < <(bash "-$-" "${script_dir}/show_cpu" load temp)
+    mapfile -t mem_info < <(bash "-$-" "${script_dir}/show_mem" --prefix GiB --round 2 mem_used mem_percent)
+    mapfile -t disk_info < <(bash "-$-" "${script_dir}/show_disk" --short-device disk_device disk_used disk_percent)
+
+    mem_info[1]="${mem_info[1]/'%'}"
+    disk_info[2]="${disk_info[2]/'%'}"
+
+    if (($(get_status_length "${cpu_info[@]}" "${mem_info[@]}" "${disk_info[@]}") < (window_size / 2))); then
+        printf -v cpu_out "| %s " "${cpu_info[@]}"
+        printf -v mem_out "| Mem: %s (%.*f%%) " "${mem_info[0]}" "0" "${mem_info[1]}"
+        printf -v disk_out "| %s: %s (%.*f%%) " "${disk_info[0]}" "${disk_info[1]}" "0" "${disk_info[2]}"
+    else
+        printf -v cpu_out "| %s " "${cpu_info[0]%% *}" "${cpu_info[@]:1}"
+        printf -v mem_out "| Mem: %.*f%% " "0" "${mem_info[1]}"
+        printf -v disk_out "| Disk: %.*f%% " "0" "${disk_info[2]}"
+    fi
+
+    printf -v time_out "| %(%a, %d %h)T | %(%H:%M)T |" "-1"
+    time_out="${time_out:-$(date '+| %a, %d %h | %H:%M |')}"
+
+    printf "%s" "${cpu_out}" "${mem_out}" "${disk_out}" "${time_out}"
 }
 
 main()
@@ -23,48 +58,12 @@ main()
     get_window_size
 
     if type -p sys-line > /dev/null 2>&1; then
-        out_1_fmt=$'| {cpu.load_avg}{cpu.temp? | {}°C}{mem.percent? | Mem: {mem.used} ({}%)}{disk.percent? | {disk.dev}: {disk.used} ({}%)} | {date.date} | {date.time} |'
-        out_2_fmt=$'| {cpu.load_avg}{cpu.temp? | {}°C}{mem.percent? | Mem: {}%}{disk.percent? | Disk: {}%} | {date.date} | {date.time} |'
-        out_1="$(sys-line --{mem,disk}-used-round=2 \
-                          --{mem,disk}-percent-round=0 \
-                          --{mem,disk}-used-prefix=GiB \
-                          --disk-short-dev \
-                          "${out_1_fmt}")"
-        if ((${#out_1} < (window_size / 2))); then
-            printf "%s" "${out_1}"
-        else
-            out_2="$(sys-line --cpu-load-short \
-                              --{mem,disk}-percent-round=0 \
-                              "${out_2_fmt}")"
-            printf "%s" "${out_2}"
-        fi
+        out="$(use_sys_line)"
     else
-        script_dir="$(type -p show_cpu)"
-        script_dir="${script_dir%/*}"
-        script_dir="${script_dir:-${HOME}/.dotfiles/scripts/info}"
-
-        mapfile -t cpu_info < <(bash "-$-" "${script_dir}/show_cpu" load temp)
-        mapfile -t mem_info < <(bash "-$-" "${script_dir}/show_mem" --prefix GiB --round 2 mem_used mem_percent)
-        mapfile -t disk_info < <(bash "-$-" "${script_dir}/show_disk" --short-device disk_device disk_used disk_percent)
-
-        mem_info[1]="${mem_info[1]/'%'}"
-        disk_info[2]="${disk_info[2]/'%'}"
-
-        if (($(get_status_length "${cpu_info[@]}" "${mem_info[@]}" "${disk_info[@]}") < (window_size / 2))); then
-            printf -v cpu_out "| %s " "${cpu_info[@]}"
-            printf -v mem_out "| Mem: %s (%.*f%%) " "${mem_info[0]}" "0" "${mem_info[1]}"
-            printf -v disk_out "| %s: %s (%.*f%%) " "${disk_info[0]}" "${disk_info[1]}" "0" "${disk_info[2]}"
-        else
-            printf -v cpu_out "| %s " "${cpu_info[0]%% *}" "${cpu_info[@]:1}"
-            printf -v mem_out "| Mem: %.*f%% " "0" "${mem_info[1]}"
-            printf -v disk_out "| Disk: %.*f%% " "0" "${disk_info[2]}"
-        fi
-
-        printf -v time_out "| %(%a, %d %h)T | %(%H:%M)T |" "-1"
-        time_out="${time_out:-$(date '+| %a, %d %h | %H:%M |')}"
-
-        printf "%s" "${cpu_out}" "${mem_out}" "${disk_out}" "${time_out}"
+        out="$(use_show_scripts)"
     fi
+
+    printf "%s" "${out}"
 }
 
 main
