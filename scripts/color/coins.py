5b7500fda539e0725aa82638e5cbdf71f5fc6616
julian-heng <julianhengwl@gmail.com>
2020-08-16 22:43:41 +0800

    General: Fix python linting

--- scripts/color/coins.py
+++ scripts/color/coins.py
@@ -1,12 +1,20 @@
 #!/usr/bin/env python3
+# pylint: disable=invalid-name,too-few-public-methods,too-many-arguments
 
-class colors:
+""" Prints coins """
+
+
+class Colors:
+    """ Contains the color ascii codes """
+
     def __init__(self):
-        self.fg = [ "\033[3{}m".format(i) for i in range(8) ]
-        self.bg = [ "\033[4{}m".format(i) for i in range(8) ]
+        self.foreground = ["\033[3{}m".format(i) for i in range(8)]
+        self.background = ["\033[4{}m".format(i) for i in range(8)]
         self.reset = "\033[0m"
 
+
 def print_box(x1, y1, x2, y2, x_off, y_off, col, pat):
+    """ Prints a box """
     length_x = abs(x2 - x1)
     length_y = abs(y2 - y1)
     length_x += 1 if length_x == 0 else 0
@@ -24,12 +32,14 @@ def print_box(x1, y1, x2, y2, x_off, y_off, col, pat):
     for i in range(length_y):
         print("\033[{};{}H{}".format(y1 + i, x1, pat * length_x))
 
+
 def main():
-    c = colors()
+    """ Main function """
+    c = Colors()
     print("\033[2J\033[H")
     count = 0
 
-    for color in c.fg:
+    for color in c.foreground:
         x_off = 14 * count
         count += 1
 
@@ -38,18 +48,20 @@ def main():
         print_box(13, 6, 13, 9, x_off, 0, color, "█")
         print_box(7, 12, 10, 12, x_off, 0, color, "█")
 
-        print_box(10, 4, 10, 11, x_off, 0, c.fg[0], "█")
-        print_box(7, 10, 10, 10, x_off, 0, c.fg[0], "█")
+        print_box(10, 4, 10, 11, x_off, 0, c.foreground[0], "█")
+        print_box(7, 10, 10, 10, x_off, 0, c.foreground[0], "█")
 
-        print_box(7, 2, 10, 2, x_off, 0, c.fg[7], "█")
-        print_box(5, 3, 7, 3, x_off, 0, c.fg[7], "█")
-        print_box(4, 4, 5, 4, x_off, 0, c.fg[7], "█")
-        print_box(7, 4, 10, 4, x_off, 0, c.fg[7], "█")
-        print_box(3, 5, 3, 9, x_off, 0, c.fg[7], "█")
-        print_box(7, 4, 7, 10, x_off, 0, c.fg[7], "█")
-        print_box(4, 9, 4, 11, x_off, 0, c.fg[7], "█")
-        print_box(5, 11, 5, 11, x_off, 0, c.fg[7], "█")
+        print_box(7, 2, 10, 2, x_off, 0, c.foreground[7], "█")
+        print_box(5, 3, 7, 3, x_off, 0, c.foreground[7], "█")
+        print_box(4, 4, 5, 4, x_off, 0, c.foreground[7], "█")
+        print_box(7, 4, 10, 4, x_off, 0, c.foreground[7], "█")
+        print_box(3, 5, 3, 9, x_off, 0, c.foreground[7], "█")
+        print_box(7, 4, 7, 10, x_off, 0, c.foreground[7], "█")
+        print_box(4, 9, 4, 11, x_off, 0, c.foreground[7], "█")
+        print_box(5, 11, 5, 11, x_off, 0, c.foreground[7], "█")
 
     print("{}\n".format(c.reset))
 
-main()
+
+if __name__ == "__main__":
+    main()
